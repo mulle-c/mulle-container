e7ead820aa7fee61ccefcab830e395d29323bff4
Nat! <nat@mulle-kybernetik.com>
2016-11-03 19:27:51 +0100

    missing script

---
+++ .bootstrap/cityhash/bin/post-fetch.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+. mulle-bootstrap-functions.sh
+
+[ ! -z "$3" ] || internal_fail "dir parameter is empty"
+[ -d "$3" ]   || fail  "directory \"$3\" is empty (`pwd -P`)"
+
+( cd "$3" ; ./configure )
+
