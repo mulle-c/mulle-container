3e6b6726ed055de98870312ced39c36f285706dc
Nat! <nat@mulle-kybernetik.com>
2017-06-14 15:37:44 +0200

    Use templates to create .md files where necessary

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,4 +1,4 @@
-PROJECT="MulleContainer"   # your project name, requires camel-case
+PROJECT="mulle-container"
 DESC="Sets, hashtables, byte buffers and more, written in C"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11
 ${DEPENDENCY_TAP}mulle-allocator'
