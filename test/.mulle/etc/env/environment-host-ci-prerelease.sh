813ab8429c274dc62786cf7f36d828c4232bbc77
Nat! <nat@mulle-kybernetik.com>
2021-07-12 14:24:57 +0200

    fix workflow

--- test/.mulle/etc/env/environment-host-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -1,4 +1,3 @@
-# Fill it with mulle-env envirionment --scope  set <key> <value>
 #
 # mulle-testallocator nodetype: ${MULLE_TESTALLOCATOR_NODETYPE:-tar}
 #
@@ -162,19 +161,19 @@ export MULLE_CONTAINER_BRANCH="prerelease"
 
 
 #
-#
+# mulle-data nodetype: ${MULLE_DATA_NODETYPE:-tar}
 #
 export MULLE_DATA_NODETYPE="git"
 
 
 #
-#
+# mulle-data url: ${MULLE_DATA_URL:-https://github.com/mulle-c/mulle-data/archive/${MULLE_TAG}.tar.gz}
 #
 export MULLE_DATA_URL="https://github.com/mulle-c/mulle-data.git"
 
 
 #
-#
+# mulle-data branch: ${MULLE_DATA_BRANCH}
 #
 export MULLE_DATA_BRANCH="prerelease"
 
